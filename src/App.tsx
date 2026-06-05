4a534eb78b81abf474783e65d4f6bc7fafebe874
mh-atlas <mhatlas.bd@gmail.com>
2026-06-05 16:46:52 +0600

    Apply corrected public version

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import AdminLogin from "./pages/AdminLogin.tsx";
-import AdminDashboard from "./pages/AdminDashboard.tsx";
 import Feedback from "./pages/Feedback.tsx";
 
 const queryClient = new QueryClient();
@@ -20,8 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/feedback" element={<Feedback />} />
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
