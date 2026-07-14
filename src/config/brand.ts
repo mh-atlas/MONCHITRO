ab5418ea5202955542c9448af1895c9a76355aae
Mental Health Atlas BD <mhatlas.bd@gmail.com>
2026-07-15 00:35:19 +0600

    Create brand.ts

---
+++ src/config/brand.ts
@@ -0,0 +1,6 @@
+export const BRAND = {
+  name: 'MONCHITRO',
+  tagline: 'Interactive mental health service mapping for Bangladesh',
+  feedbackSource: 'MONCHITRO feedback route',
+  feedbackStorageKey: 'monchitro_feedback_v1',
+} as const;
